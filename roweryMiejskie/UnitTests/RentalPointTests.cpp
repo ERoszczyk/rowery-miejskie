77ddf7c176cdc93687d1466c54a10fac735dd915
Kinga Zofia Kwoka <k.kwoka@stud.elka.pw.edu.pl>
2020-04-27 16:43:04 +0200

    Add RentalPoint Tests

---
+++ roweryMiejskie/UnitTests/RentalPointTests.cpp
@@ -0,0 +1,93 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+//#include "..\roweryMiejskie\RentalPoint.cpp"
+#include "..\roweryMiejskie\RentalPoint.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace RentalTests
+{
+	TEST_CLASS(TestRentalPoint)
+	{
+	public:
+		TEST_METHOD(TestConstructor) //Constructor if a bike added is rented
+		{
+			BikeDatabase base(5);
+			base.setBikeState(2, true);
+			RentalPoint point(base);
+			int size = point.getBikes().size();
+			int free = point.getFreeBikes().size();
+			Assert::AreEqual(size, 5);
+			Assert::AreEqual(free, 4);
+
+		};
+		TEST_METHOD(TestConstructor2) //Constructor if database too big
+		{
+			BikeDatabase base(15);
+			RentalPoint point(base);
+			int size = point.getBikes().size();
+			Assert::AreEqual(size, 10);  //10 set as max rental point size
+		};
+		TEST_METHOD(TestConstructor3) //Constructor if too many bikeIds
+		{
+			std::vector<int>bikes = { 1,2,3,4,5,6,7,8,9,10,11,12 };
+			RentalPoint point(bikes);
+			int size = point.getBikes().size();
+			Assert::AreEqual(size, 10);
+		};
+		TEST_METHOD(TestRentalAdd) // Unacceptable Id
+		{
+			RentalPoint point;
+			point.addBike(-3);
+			int size = point.getBikes().size();
+			Assert::AreEqual(size, 0);
+		};
+		TEST_METHOD(TestAddBikes) // Too many bikes
+		{
+			std::vector<int>bikes = { 1,2,3,4,5,6,7,8,9,10,11,12 };
+			RentalPoint point;
+			point.addBikes(bikes);
+			int size = point.getBikes().size();
+			Assert::AreEqual(size, 10);
+		};
+		TEST_METHOD(TestRemoveBikes)//Inexistent bike
+		{
+			std::vector<int>bikes = { 1,2,3 };
+			std::vector<int>otherBikes = { 2,4 };
+			RentalPoint point;
+			point.addBikes(bikes);
+			point.removeBikes(otherBikes);
+			int size = point.getBikes().size();
+			Assert::AreEqual(size, 2);
+		};
+		TEST_METHOD(TestRent)  //old test
+		{
+			BikeDatabase base(5);
+			RentalPoint point(base);
+			User user("a", "b", "c", "d");
+			point.rent(4, 1, base, user);
+			Assert::IsTrue(base.getBikeState(4));
+			Assert::AreEqual(base.getBikeOwner(4), 1);
+			Assert::AreEqual(base.getBikeStand(4), 0);
+			std::vector<int> free = point.getFreeBikes();
+			Assert::IsTrue(find(free.begin(), free.end(), 4) == free.end());
+
+		};
+		//TEST_METHOD(TestPutBack) //old test, now requires user input for choosing bike stand
+		//{
+		//	BikeDatabase base(5);
+		//	RentalPoint point(base);
+		//	User user("a", "b", "c", "d");
+		//	point.rent(4, 1, base, user);
+		//	point.putBack(4, 1, base, user);
+		//	Assert::IsFalse(base.getBikeState(4));
+		//	Assert::AreEqual(base.getBikeOwner(4), 0);
+		//	Assert::AreEqual(base.getBikeStand(4), 1);
+		//	std::vector<int> free = point.getFreeBikes();
+		//	Assert::IsTrue(find(free.begin(), free.end(), 4) != free.end());
+
+		//}
+
+
+	};
+}
